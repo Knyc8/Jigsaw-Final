2f974df4013c01e3834df2e8c4f909bd9352b595
knyc8 <kchen9712@bths.edu>
2024-01-12 15:14:08 -0500

    asd

--- scripting.js
+++ scripting.js
@@ -1,1 +1,10 @@
-//In progress
+const puzzleImg = document.getElementById('puzzlefield');
+
+var loadFile = (event) => {
+    var imgUrl = URL.createObjectURL(event.target.files[0]);
+
+    puzzleImg.style.backgroundImage = `url(${imgUrl})`;
+
+    puzzleImg.style.height = "40rem";
+    puzzleImg.style.width = "50rem";
+}
